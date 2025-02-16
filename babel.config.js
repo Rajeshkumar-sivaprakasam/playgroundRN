0790a2af4e02762173b21c9ff950a0c77da5f162
rajesh <rk.sivaprakasam@outlook.com>
2025-02-16 12:21:45 +0530

    feat: configuration for prettier,eslint,commitlint,husky without notes

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  presets: ["module:@react-native/babel-preset"],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
-      "module-resolver",
+      'module-resolver',
       {
-        root: ["./src"],
-        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          "@shared": "./src/shared/*",
-          "@screens": "./src/screens/*",
+          '@shared': './src/shared/*',
+          '@screens': './src/screens/*',
         },
       },
     ],
   ],
-};
+}
